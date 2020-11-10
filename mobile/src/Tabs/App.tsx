18521e85fecff317583e70f03181e7bf3430e384
Maycom Willams <74081416+Maycomwill@users.noreply.github.com>
2020-11-10 15:48:56 -0300

    Update da página inicial
    Organização do tamanho das Tabs

--- mobile/src/Tabs/App.tsx
+++ mobile/src/Tabs/App.tsx
@@ -1,57 +1,40 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import {
-  Container,
-  TabsContainer,
-  TabItem,
-  TabText,
-  IconList,
-  CustomButton,
-} from './styles';
+import { Container, TabsContainer, TabItem, TabText, IconList } from './styles';
 
 const Tabs: React.FC = () => {
   return (
     <Container>
       <TabsContainer>
-        <TabItem>
-          <CustomButton onPress={() => alert('Pessoal')}>
-            <TabText>Pessoal</TabText>
-            <IconList>
-              <Icon name="person-outline" size={40} color="#fff" />
-            </IconList>
-          </CustomButton>
+        <TabItem onPress={() => alert('Pessoal')}>
+          <TabText>Pessoal</TabText>
+          <IconList>
+            <Icon name="person-outline" size={45} color="#fff" />
+          </IconList>
         </TabItem>
-        <TabItem>
-          <CustomButton onPress={() => alert('Trabalho')}>
-            <TabText>Trabalho</TabText>
-            <IconList>
-              <Icon name="work-outline" size={40} color="#fff" />
-            </IconList>
-          </CustomButton>
+        <TabItem onPress={() => alert('Trabalho')}>
+          <TabText>Trabalho</TabText>
+          <IconList>
+            <Icon name="work-outline" size={45} color="#fff" />
+          </IconList>
         </TabItem>
-        <TabItem>
-          <CustomButton onPress={() => alert('Faculdade')}>
-            <TabText>Faculdade</TabText>
-            <IconList>
-              <Icon name="library-books" size={40} color="#fff" />
-            </IconList>
-          </CustomButton>
+        <TabItem onPress={() => alert('Faculdade')}>
+          <TabText>Faculdade</TabText>
+          <IconList>
+            <Icon name="library-books" size={45} color="#fff" />
+          </IconList>
         </TabItem>
-        <TabItem>
-          <CustomButton onPress={() => alert('Compras')}>
-            <TabText>Compras</TabText>
-            <IconList>
-              <Icon name="shopping-cart" size={40} color="#fff" />
-            </IconList>
-          </CustomButton>
+        <TabItem onPress={() => alert('Compras')}>
+          <TabText>Compras</TabText>
+          <IconList>
+            <Icon name="shopping-cart" size={45} color="#fff" />
+          </IconList>
         </TabItem>
-        <TabItem>
-          <CustomButton onPress={() => alert('Viagem')}>
-            <TabText>Viagem</TabText>
-            <IconList>
-              <Icon name="flight" size={40} color="#fff" />
-            </IconList>
-          </CustomButton>
+        <TabItem onPress={() => alert('Viagem')}>
+          <TabText>Viagem</TabText>
+          <IconList>
+            <Icon name="flight" size={45} color="#fff" />
+          </IconList>
         </TabItem>
       </TabsContainer>
     </Container>
--- mobile/src/Tabs/styles.ts
+++ mobile/src/Tabs/styles.ts
@@ -12,29 +12,32 @@ export const TabsContainer = styled.ScrollView.attrs({
   showsHorizontalScrollIndicator: false,
   pagingEnabled: true,
 })`
-margin: 0px 10px 0px;
+  margin: 0px 10px 0px;
 `;
 
-export const TabItem = styled.View`
+export const TabItem = styled.TouchableOpacity`
   box-shadow: 0 5px 15px rgba(0, 0, 0, 1);
   flex-direction: row;
   margin: 0px 10px 0px;
   border-radius: 15px;
-  width: 200px;
+  width: 370px;
   height: 100px;
   background-color: #051956;
+  justify-content: flex-end;
   align-items: center;
-  justify-content: space-evenly;
 `;
 
 export const TabText = styled.Text`
   font-weight: bold;
-  margin: -50px 10px 0;
+  align-self: center;
+  margin-bottom: 48px;
+  margin-right: 45px;
   color: white;
-  font-size: 15px;
+  font-size: 18px;
 `;
 export const IconList = styled.View`
-  margin: -50px 5px 0;
+  margin-bottom: 50px;
+  margin-right: 60px;
 `;
 export const CustomButton = styled.TouchableOpacity`
   box-shadow: 0 5px 15px rgba(0, 0, 0, 1);
